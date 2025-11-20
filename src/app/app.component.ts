4ed26f856232800193486783928ff3b1d30971ee
vishnutt <vishnutt@xminds.com>
2025-11-20 18:14:35 +0530

    button color example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,4 +14,8 @@ export class AppComponent  {
   callChildMethod() {
     this.child.sayHello();
   }
+
+onParentButtonClick() {
+  this.child.highlightRed();
+}
 }
--- src/app/button-child/button-child.component.ts
+++ src/app/button-child/button-child.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-button-child',
   templateUrl: './button-child.component.html',
   styleUrls: ['./button-child.component.css']
 })
-export class ButtonChildComponent {
+export class ButtonChildComponent implements AfterViewInit {
+
+ngAfterViewInit(): void {
+
+}
 
   theText: string = '';
 
@@ -13,6 +17,12 @@ export class ButtonChildComponent {
   sayHello() {
     console.log('Hello from Child Component!');
     this.theText = 'Hello from Child Component!';
+  }
+
+   @ViewChild('childBtn') button!: ElementRef;
+
+  highlightRed() {
+    this.button.nativeElement.style.background = 'red';
   }
 
 }
