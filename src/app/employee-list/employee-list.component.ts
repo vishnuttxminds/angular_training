1f2548d4a6d6e86d6331378d4686c26d55d5d016
vishnutt <vishnutt@xminds.com>
2025-11-20 11:22:17 +0530

    child to parent  @Output()

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -4,13 +4,22 @@ import { Employee } from '../models/employee.model';
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
-  styleUrls: ['./employee-list.component.css']
+  styleUrls: ['./employee-list.component.css'],
 })
 export class EmployeeListComponent {
-
-   employees: Employee[] = [
+  employees: Employee[] = [
     { id: 1, name: 'John', role: 'Manager', salary: 10000 },
     { id: 2, name: 'Sara', role: 'Developer', salary: 15000 },
-    { id: 3, name: 'Ravi', role: 'Designer', salary: 220000 }
+    { id: 3, name: 'Ravi', role: 'Designer', salary: 220000 },
   ];
+  childMessage: string = '';
+  childMessage2: string = '';
+
+  receiveMessage(msg: string) {
+    this.childMessage = msg;
+  }
+
+  receiveMessage2(msg: string) {
+    this.childMessage2 = msg;
+  }
 }
--- src/app/employee-list/employee-view/employee-view.component.ts
+++ src/app/employee-list/employee-view/employee-view.component.ts
@@ -1,12 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Employee } from 'src/app/models/employee.model';
-
 
 @Component({
   selector: 'app-employee-view',
   templateUrl: './employee-view.component.html',
-  styleUrls: ['./employee-view.component.css']
+  styleUrls: ['./employee-view.component.css'],
 })
 export class EmployeeViewComponent {
-  @Input() employee!: Employee;  
+  @Input() employee!: Employee;
+
+  // child to parent data transfer @output
+  @Output() messageEvent = new EventEmitter<string>();
+  @Output() messageEvent2 = new EventEmitter<string>();
+
+  ngOnInit() {
+    this.messageEvent.emit('Hello from Child Component!');
+    this.messageEvent2.emit('Hello from Child Component 2222');
+  }
 }
