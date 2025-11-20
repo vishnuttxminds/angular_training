16473fba7b678541a88785c5f8bf9642039d925c
vishnutt <vishnutt@xminds.com>
2025-11-20 18:38:43 +0530

    chile component elelemt access , same component elemet access using @ViewChild

--- src/app/button-child/button-child.component.ts
+++ src/app/button-child/button-child.component.ts
@@ -3,26 +3,22 @@ import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 @Component({
   selector: 'app-button-child',
   templateUrl: './button-child.component.html',
-  styleUrls: ['./button-child.component.css']
+  styleUrls: ['./button-child.component.css'],
 })
 export class ButtonChildComponent implements AfterViewInit {
-
-ngAfterViewInit(): void {
-
-}
-
+  ngAfterViewInit(): void {}
   theText: string = '';
-
-
   sayHello() {
     console.log('Hello from Child Component!');
     this.theText = 'Hello from Child Component!';
   }
-
-   @ViewChild('childBtn') button!: ElementRef;
+  @ViewChild('childBtn') button!: ElementRef;
 
   highlightRed() {
     this.button.nativeElement.style.background = 'red';
   }
 
+  clickMe() {
+    this.button.nativeElement.style.background = 'blue';
+  }
 }
